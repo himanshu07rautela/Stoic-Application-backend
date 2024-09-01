2953584d8a5596d0e22aa227c4fc9ef1aad84b6a
himanshu <rohithaaris01072003@gmail.com>
2024-09-01 12:25:00 +0530

    real react url added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173', // Replace with your React app's URL
+  origin: 'https://stoic-application-frontend.vercel.app/', // Replace with your React app's URL
 }));
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI)
